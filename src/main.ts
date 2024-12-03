e443b109366da94e78b76472a534d632f155c3d1
anna <ana.pego@estudante.ifms.edu.br>
2024-12-03 11:13:45 -0400

    atualização

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,9 @@ import mysql from 'mysql2/promise'
 const app = express()
 
 app.use(express.json())  
-
 app.use(cors())
 
-app.get("/chocolates",async(req,res)=>{
+app.get("/chocolates",async(req,res)=>{  
 
     try{
         const conexao = await mysql.createConnection({
@@ -18,13 +17,12 @@ app.get("/chocolates",async(req,res)=>{
             database:process.env.dbname?process.env.dbname:"cacaushow",
             port:process.env.dbport?parseInt(process.env.dbport):3306
         })
-        
+       
         const [result,fields]  = await conexao.query("SELECT * FROM chocolates")
         await conexao.end()
-     
+    
         res.send(result)
     }catch(e){
-        console.log(e)
         res.status(500).send("Erro do servidor")
     }
 });
@@ -58,10 +56,10 @@ app.get("/cliente",async(req,res)=>{
             database:process.env.dbname?process.env.dbname:"cacaushow",
             port:process.env.dbport?parseInt(process.env.dbport):3306
         })
-        
+    
         const [result,fields]  = await conexao.query("SELECT * FROM cliente")
         await conexao.end()
-     
+
         res.send(result)
     }catch(e){
         console.log(e)
@@ -74,12 +72,12 @@ app.post("/cliente",async(req,res)=>{
         const conexao = await mysql.createConnection({
             host: process.env.dbhost?process.env.dbhost:"localhost",
             user:process.env.dbuser?process.env.dbuser:"root",
-            password:process.env.dbpassword?process.env.dbpassword:"",
+            password:process.env.dbpassword?process.env.dbpassword:"", 
             database:process.env.dbname?process.env.dbname:"cacaushow",
             port:process.env.dbport?parseInt(process.env.dbport):3306
         })
         const {clienteId,nome,cpf} = req.body
-        const [result,fields]  = await conexao.query("INSERT INTO cliente VALUES (?,?,?,?,?)",[clienteId,nome,cpf])
+        const [result,fields]  = await conexao.query("INSERT INTO cliente VALUES (?,?,?)",[clienteId,nome,cpf])
         await conexao.end()
         res.send(result)
     }catch(e){
@@ -98,10 +96,10 @@ app.get("/pagamento",async(req,res)=>{
             database:process.env.dbname?process.env.dbname:"cacaushow",
             port:process.env.dbport?parseInt(process.env.dbport):3306
         })
-        
+    
         const [result,fields]  = await conexao.query("SELECT * FROM pagamento")
         await conexao.end()
-     
+
         res.send(result)
     }catch(e){
         console.log(e)
@@ -119,7 +117,7 @@ app.post("/pagamento",async(req,res)=>{
             port:process.env.dbport?parseInt(process.env.dbport):3306
         })
         const {idpagamento,formapag,descricao,valor} = req.body
-        const [result,fields]  = await conexao.query("INSERT INTO cliente VALUES (?,?,?,?,?)",[idpagamento,formapag,descricao,valor])
+        const [result,fields]  = await conexao.query("INSERT INTO pagamento VALUES (?,?,?,?)",[idpagamento,formapag,descricao,valor])
         await conexao.end()
         res.send(result)
     }catch(e){
@@ -127,6 +125,7 @@ app.post("/pagamento",async(req,res)=>{
         res.status(500).send("Erro do servidor")
     }
 });
+
 
 app.get("/estoque",async(req,res)=>{
 
@@ -138,10 +137,10 @@ app.get("/estoque",async(req,res)=>{
             database:process.env.dbname?process.env.dbname:"cacaushow",
             port:process.env.dbport?parseInt(process.env.dbport):3306
         })
-        
+    
         const [result,fields]  = await conexao.query("SELECT * FROM estoque")
         await conexao.end()
-     
+
         res.send(result)
     }catch(e){
         console.log(e)
@@ -159,7 +158,7 @@ app.post("/estoque",async(req,res)=>{
             port:process.env.dbport?parseInt(process.env.dbport):3306
         })
         const {itemId,nomeProduto,quantidade,localizacao} = req.body
-        const [result,fields]  = await conexao.query("INSERT INTO cliente VALUES (?,?,?,?,?)",[itemId,nomeProduto,quantidade,localizacao])
+        const [result,fields]  = await conexao.query("INSERT INTO estoque VALUES (?,?,?,?)",[itemId,nomeProduto,quantidade,localizacao])
         await conexao.end()
         res.send(result)
     }catch(e){
@@ -178,10 +177,10 @@ app.get("/promocoes",async(req,res)=>{
             database:process.env.dbname?process.env.dbname:"cacaushow",
             port:process.env.dbport?parseInt(process.env.dbport):3306
         })
-        
+    
         const [result,fields]  = await conexao.query("SELECT * FROM promocoes")
         await conexao.end()
-     
+
         res.send(result)
     }catch(e){
         console.log(e)
@@ -199,7 +198,7 @@ app.post("/promocoes",async(req,res)=>{
             port:process.env.dbport?parseInt(process.env.dbport):3306
         })
         const {idpromocao,titulo,descricao,validade,cupom} = req.body
-        const [result,fields]  = await conexao.query("INSERT INTO cliente VALUES (?,?,?,?,?)",[idpromocao,titulo,descricao,validade,cupom])
+        const [result,fields]  = await conexao.query("INSERT INTO promocoes VALUES (?,?,?,?,?)",[idpromocao,titulo,descricao,validade,cupom])
         await conexao.end()
         res.send(result)
     }catch(e){
